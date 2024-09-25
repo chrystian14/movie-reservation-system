46ea2dd38e10f95cf81e5ecc4f3f3c03c4ec578e
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-09-25 06:55:50 -0300

    feat: :heavy_plus_sign: add morgan logger package and middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 export const app = express();
+
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
