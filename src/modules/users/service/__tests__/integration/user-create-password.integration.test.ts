024ed544649646d5794494625647a6c4939dc225
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-01 03:11:55 -0300

    feat: :sparkles: add user password hashing integration tests

---
+++ src/modules/users/service/__tests__/integration/user-create-password.integration.test.ts
@@ -0,0 +1,49 @@
+import { clearDatabase } from "../../../../../configs/jest-setup.config";
+import { prisma } from "../../../../../configs/prisma-client.config";
+import { UserCreateInput } from "../../../types";
+import { UserService } from "../../user.service";
+
+describe("INTEGRATION: UserService.create password hash", () => {
+  let userService: UserService;
+  let userCreateInput: UserCreateInput;
+
+  beforeEach(() => {
+    userService = new UserService();
+
+    userCreateInput = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      password: "password",
+    };
+  });
+
+  afterEach(async () => {
+    await clearDatabase();
+  });
+
+  it("should create a user with a password hash", async () => {
+    const plainPassword = userCreateInput.password;
+    const user = await userService.create(userCreateInput);
+
+    const createdUser = await prisma.user.findUnique({
+      where: { email: userCreateInput.email },
+    });
+
+    expect(createdUser).toBeDefined();
+    expect(createdUser?.password).not.toBe(plainPassword);
+
+    const isPasswordMatch = await UserService.comparePassword(
+      plainPassword,
+      createdUser?.password!
+    );
+
+    expect(isPasswordMatch).toBe(true);
+  });
+
+  test("should return user data without password", async () => {
+    const createdUser = await userService.create(userCreateInput);
+
+    expect(Object.keys(createdUser)).not.toContain("password");
+  });
+});
