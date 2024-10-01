a35d7580f79343cca48c8c44fbabd595fca82596
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-01 17:49:56 -0300

    refactor: :recycle: refactor user repository methods to use native function instead of arrow function due to bind issues

--- src/modules/users/repository/user.repository.ts
+++ src/modules/users/repository/user.repository.ts
@@ -4,17 +4,25 @@ import type { IUserRepository } from "./user.repository.interface";
 import { prisma } from "configs/prisma-client.config";
 
 export class UserRepository implements IUserRepository {
-  create = async (userCreateInput: UserCreateInput): Promise<User> => {
+  async create(userCreateInput: UserCreateInput): Promise<User> {
+    console.log("#".repeat(20));
+    console.log("UserRepository.create CHAMADO!!!");
+    console.log("#".repeat(20));
+
     const user = await prisma.user.create({
       data: userCreateInput,
     });
 
     return user;
-  };
+  }
 
-  countByEmail = async (email: string): Promise<number> => {
+  async countByEmail(email: string): Promise<number> {
+    console.log("#".repeat(20));
+    console.log("UserRepository.countByEmail CHAMADO!!!");
+    console.log("#".repeat(20));
+
     return await prisma.user.count({
       where: { email },
     });
-  };
+  }
 }
