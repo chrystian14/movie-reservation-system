5f5c02cb48a49e81fe0553ef34150407ba0daf65
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-02 14:23:53 -0300

    test: :test_tube: add user controller create integration test with valid data

---
+++ src/modules/users/controller/__tests__/integration/user-create-controller.integration.test.ts
@@ -0,0 +1,45 @@
+import { apiClient, clearDatabase } from "configs/jest-setup.config";
+import { prisma } from "configs/prisma-client.config";
+import { type UserCreateInput } from "modules/users/types";
+
+describe("INTEGRATION: UserControler.create", () => {
+  let userCreateInput: UserCreateInput;
+  const userEndpoint = "/api/v1/users";
+
+  beforeEach(async () => {
+    await clearDatabase();
+
+    userCreateInput = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      password: "johndoepassword",
+    };
+  });
+
+  afterEach(async () => {
+    await clearDatabase();
+  });
+
+  test("should create a regular user and return user data without password", async () => {
+    const response = await apiClient.post(userEndpoint).send(userCreateInput);
+
+    expect(response.status).toBe(201);
+    expect(response.body).not.toHaveProperty("password");
+
+    const expectedResponseBody = {
+      id: expect.any(String),
+      firstName: userCreateInput.firstName,
+      lastName: userCreateInput.lastName,
+      email: userCreateInput.email,
+      isAdmin: false,
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    };
+
+    expect(response.body).toStrictEqual(expectedResponseBody);
+
+    const userCount = await prisma.user.count();
+    expect(userCount).toBe(1);
+  });
+});
