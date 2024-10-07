eea62d43a7cc4f062f896016cf580593d57cf18a
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-07 15:34:42 -0300

    refactor: :recycle: update auth service to compare password hash

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -2,14 +2,21 @@ import type { IUserRepository } from "modules/users/repository";
 import type { LoginInput } from "../types";
 import { InvalidCredentialsError } from "../errors";
 import type { IAuthService } from "./auth.service.interface";
+import { comparePassword } from "modules/users/utils";
 
 export class AuthService implements IAuthService {
   constructor(private userRepository: IUserRepository) {}
 
-  async login(loginData: LoginInput) {
-    const user = await this.userRepository.findByEmail(loginData.email);
+  async login({ email, password }: LoginInput) {
+    const user = await this.userRepository.findByEmail(email);
 
     if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const passwordMatch = await comparePassword(password, user.password);
+
+    if (!passwordMatch) {
       throw new InvalidCredentialsError();
     }
 
