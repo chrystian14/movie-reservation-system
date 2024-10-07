ce28780e8d2fb3908e11b037814e5ae9cf4b379d
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-07 15:33:52 -0300

    test: :test_tube: add user login with non matching password test

--- src/modules/auth/service/__tests__/unit/login.unit.test.ts
+++ src/modules/auth/service/__tests__/unit/login.unit.test.ts
@@ -1,5 +1,7 @@
 import { AuthService } from "modules/auth/service";
+import { UserBuilder } from "modules/users/builder";
 import { UserRepository, type IUserRepository } from "modules/users/repository";
+import * as passwordHashing from "modules/users/utils/password-hashing";
 
 jest.mock("modules/users/repository/user.repository.ts");
 
@@ -28,6 +30,35 @@ describe("UNIT: AuthService.login", () => {
     expect(mockedUserRepository.findByEmail).toHaveBeenCalledTimes(1);
     expect(mockedUserRepository.findByEmail).toHaveBeenCalledWith(
       nonRegisteredEmailLoginData.email
+    );
+  });
+
+  test("should throw an error if login-in with non matching password", async () => {
+    const mockedUser = new UserBuilder().build();
+    mockedUserRepository.findByEmail.mockResolvedValueOnce(mockedUser);
+
+    const comparePasswordSpy = jest
+      .spyOn(passwordHashing, "comparePassword")
+      .mockResolvedValueOnce(false);
+
+    const nonMatchingPasswordLoginData = {
+      email: "registered-email@test.com",
+      password: "1234567",
+    };
+
+    await expect(
+      authService.login(nonMatchingPasswordLoginData)
+    ).rejects.toThrow("Invalid credentials");
+
+    expect(mockedUserRepository.findByEmail).toHaveBeenCalledTimes(1);
+    expect(mockedUserRepository.findByEmail).toHaveBeenCalledWith(
+      nonMatchingPasswordLoginData.email
+    );
+
+    expect(comparePasswordSpy).toHaveBeenCalledTimes(1);
+    expect(comparePasswordSpy).toHaveBeenCalledWith(
+      nonMatchingPasswordLoginData.password,
+      mockedUser.password
     );
   });
 });
