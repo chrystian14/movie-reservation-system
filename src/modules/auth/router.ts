c797a71e5a96749db0f49fc27a3da36e4c81a01b
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-08 13:14:32 -0300

    feat: :sparkles: add validation to login route

--- src/modules/auth/router.ts
+++ src/modules/auth/router.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { AuthController } from "./controller";
 import { AuthService } from "./service";
 import { UserRepository } from "modules/users/repository";
+import { validateBody } from "modules/_shared/middlewares";
+import { loginInputSchema } from "./types/schemas";
 
 export const authRouter = Router();
 
@@ -9,4 +11,4 @@ const userRepository = new UserRepository();
 const authService = new AuthService(userRepository);
 const authController = new AuthController(authService);
 
-authRouter.post("", authController.login);
+authRouter.post("", validateBody(loginInputSchema), authController.login);
