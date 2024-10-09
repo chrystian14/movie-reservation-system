c6a34e6b15b2fa4cd1368f2801164e4e1681f6e0
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-09 02:43:04 -0300

    refactor: :recycle: added custom response locals type for protected routes

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,11 +1,12 @@
 import { status } from "modules/_shared/utils";
 import type { IAuthService } from "../service";
-import type { Request, Response } from "express";
+import type { Request } from "express";
+import type { AutheticatedResponse } from "../types";
 
 export class AuthController {
   constructor(private readonly authService: IAuthService) {}
 
-  login = async (req: Request, res: Response) => {
+  login = async (req: Request, res: AutheticatedResponse) => {
     const token = await this.authService.login(req.body);
 
     return res.status(status.HTTP_200_OK).json({ access_token: token });
--- src/modules/auth/types/index.ts
+++ src/modules/auth/types/index.ts
@@ -1,5 +1,15 @@
 import type z from "zod";
 import type { loginInputSchema } from "./schemas";
+import type { Response } from "express";
 
 export type LoginInput = z.infer<typeof loginInputSchema>;
 export type JwtGeneratedTokenPayload = { isAdmin: boolean };
+
+type AuthenticatedResLocals = {
+  verifiedToken: string;
+};
+
+export type AutheticatedResponse<T = unknown> = Response<
+  T,
+  AuthenticatedResLocals
+>;
