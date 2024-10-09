acb4f898cfbd09caa03aa02199f4ad1df9100668
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-09 18:04:29 -0300

    refactor: :recycle: update movie create integration test to use user builder

--- src/modules/movies/controller/__tests__/integration/movie-create-controller.integration.test.ts
+++ src/modules/movies/controller/__tests__/integration/movie-create-controller.integration.test.ts
@@ -2,6 +2,7 @@ import { clearDatabase } from "configs/jest-setup.config";
 import { prisma } from "configs/prisma-client.config";
 import { apiClient } from "modules/_shared/tests";
 import { status } from "modules/_shared/utils";
+import { generateToken } from "modules/auth/jwt";
 import { GenreBuilder } from "modules/genres/builder";
 import {
   GenreRepository,
@@ -10,6 +11,8 @@ import {
 import type { Genre } from "modules/genres/types";
 import { MovieBuilder } from "modules/movies/builder";
 import { type Movie, type MovieCreateInput } from "modules/movies/types";
+import { UserBuilder } from "modules/users/builder";
+import { UserRepository } from "modules/users/repository";
 
 describe("INTEGRATION: MovieControler.create - POST /api/v1/movies", () => {
   const movieEndpoint = "/api/v1/movies";
@@ -21,6 +24,9 @@ describe("INTEGRATION: MovieControler.create - POST /api/v1/movies", () => {
   let genreRepository: IGenreRepository;
   let createdGenre: Genre;
 
+  let regularUserToken: string;
+  let adminUsertoken: string;
+
   beforeEach(async () => {
     await clearDatabase();
 
@@ -29,10 +35,18 @@ describe("INTEGRATION: MovieControler.create - POST /api/v1/movies", () => {
     createdGenre = await genreBuilder.save(genreRepository);
 
     movieBuilder = new MovieBuilder();
-
     movieWithValidGenre = movieBuilder
       .withGenreId(createdGenre.id)
       .requiredForCreation();
+
+    const userRepository = new UserRepository();
+    const regularUserBuilder = new UserBuilder().withNonAdminRole();
+    const regularUser = await regularUserBuilder.save(userRepository);
+    regularUserToken = generateToken(regularUser);
+
+    const adminUserBuilder = new UserBuilder().withAdminRole();
+    const adminUser = await adminUserBuilder.save(userRepository);
+    adminUsertoken = generateToken(adminUser);
   });
 
   test("should return a 401 when user is not authenticated", async () => {
@@ -51,9 +65,27 @@ describe("INTEGRATION: MovieControler.create - POST /api/v1/movies", () => {
     expect(movieCount).toBe(0);
   });
 
-  test("should create a movie", async () => {
+  test("should return a 403 when user is authenticated but not an admin", async () => {
     const response = await apiClient
       .post(movieEndpoint)
+      .set("Authorization", `Bearer ${regularUserToken}`)
+      .send(movieWithValidGenre);
+
+    const expectedResponseBody = {
+      details: "You don't have permission to perform this action",
+    };
+
+    expect(response.statusCode).toBe(status.HTTP_403_FORBIDDEN);
+    expect(response.body).toEqual(expectedResponseBody);
+
+    const movieCount = await prisma.movie.count();
+    expect(movieCount).toBe(0);
+  });
+
+  test("should create a movie if user is admin", async () => {
+    const response = await apiClient
+      .post(movieEndpoint)
+      .set("Authorization", `Bearer ${adminUsertoken}`)
       .send(movieWithValidGenre);
 
     expect(response.status).toBe(status.HTTP_201_CREATED);
