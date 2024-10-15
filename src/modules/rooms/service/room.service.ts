2ee94948782ead92cd312c48a08712e2abdab1f3
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-15 13:36:15 -0300

    refactor: :recycle: update room service create to create seats based on rows and columns of the room

--- src/modules/rooms/service/room.service.ts
+++ src/modules/rooms/service/room.service.ts
@@ -1,3 +1,4 @@
+import type { SeatWithoutRoomId } from "modules/seats/types";
 import type { IRoomRepository } from "../repository";
 import type { Room, RoomCreateInput } from "../types";
 import type { IRoomService } from "./room.service.interface";
@@ -6,6 +7,20 @@ export class RoomService implements IRoomService {
   constructor(private readonly roomRepository: IRoomRepository) {}
 
   async create(roomCreateInput: RoomCreateInput): Promise<Room> {
-    return await this.roomRepository.create(roomCreateInput);
+    const seats: Array<SeatWithoutRoomId> = [];
+
+    for (let row = 0; row < roomCreateInput.rows; row++) {
+      for (let column = 0; column < roomCreateInput.columns; column++) {
+        const currentSeat: SeatWithoutRoomId = {
+          column: String.fromCharCode(97 + column),
+          row: row + 1,
+          price: roomCreateInput.baseSeatPrice,
+        };
+
+        seats.push(currentSeat);
+      }
+    }
+
+    return await this.roomRepository.createWithSeats(roomCreateInput, seats);
   }
 }
