527ad381c230cc5a840b1a2bb09c857cac93cab6
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-15 16:55:06 -0300

    test: :white_check_mark: add showtime controller create integration tests for  non existing roomId and movieId

--- src/modules/showtimes/controller/__tests__/integration/showtime-create-controller.integration.test.ts
+++ src/modules/showtimes/controller/__tests__/integration/showtime-create-controller.integration.test.ts
@@ -1,7 +1,15 @@
+import type { Movie, Room } from "@prisma/client";
 import { clearDatabase } from "configs/jest-setup.config";
+import { randomUUID } from "crypto";
 import { apiClient } from "modules/_shared/tests";
 import { status } from "modules/_shared/utils";
 import { generateToken } from "modules/auth/jwt";
+import { GenreBuilder } from "modules/genres/builder";
+import { GenreRepository } from "modules/genres/repository";
+import { MovieBuilder } from "modules/movies/builder";
+import { MovieRepository } from "modules/movies/repository";
+import { RoomBuilder } from "modules/rooms/builder";
+import { RoomRepository } from "modules/rooms/repository";
 import { ShowtimeBuilder } from "modules/showtimes/builder";
 import {
   ShowtimeRepository,
@@ -14,30 +22,44 @@ import { UserRepository } from "modules/users/repository";
 describe("INTEGRATION: ShowtimeControler.create - POST /api/v1/showtimes", () => {
   const showtimeEndpoint = "/api/v1/showtimes";
 
+  let showtimeRepository: IShowtimeRepository;
   let showtimeBuilder: ShowtimeBuilder;
   let showtimeCreateInput: ShowtimeCreateInput;
 
   let regularUserToken: string;
   let adminUserToken: string;
 
-  let showtimeRepository: IShowtimeRepository;
+  let createdMovie: Movie;
+  let createdRoom: Room;
 
   beforeEach(async () => {
     await clearDatabase();
 
-    showtimeRepository = new ShowtimeRepository();
-
-    showtimeBuilder = new ShowtimeBuilder();
-    showtimeCreateInput = showtimeBuilder.requiredForCreation();
-
     const userRepository = new UserRepository();
-    const regularUserBuilder = new UserBuilder().withNonAdminRole();
-    const regularUser = await regularUserBuilder.save(userRepository);
+    const regularUser = await new UserBuilder()
+      .withNonAdminRole()
+      .save(userRepository);
     regularUserToken = generateToken(regularUser);
 
-    const adminUserBuilder = new UserBuilder().withAdminRole();
-    const adminUser = await adminUserBuilder.save(userRepository);
+    const adminUser = await new UserBuilder()
+      .withAdminRole()
+      .save(userRepository);
     adminUserToken = generateToken(adminUser);
+
+    const createdGenre = await new GenreBuilder().save(new GenreRepository());
+
+    createdMovie = await new MovieBuilder()
+      .withGenreId(createdGenre.id)
+      .save(new MovieRepository());
+
+    createdRoom = await new RoomBuilder().save(new RoomRepository());
+
+    showtimeRepository = new ShowtimeRepository();
+    showtimeBuilder = new ShowtimeBuilder();
+    showtimeCreateInput = showtimeBuilder
+      .withMovieId(createdMovie.id)
+      .withRoomId(createdRoom.id)
+      .requiredForCreation();
   });
 
   test("should return a 401 when user is not authenticated", async () => {
@@ -73,7 +95,7 @@ describe("INTEGRATION: ShowtimeControler.create - POST /api/v1/showtimes", () =>
     expect(showtimeCount).toBe(0);
   });
 
-  test("should return an error when creating a showtime with admin user but without required fields", async () => {
+  test("should return a 400 when creating a showtime with admin user but without required fields", async () => {
     const response = await apiClient
       .post(showtimeEndpoint)
       .set("Authorization", `Bearer ${adminUserToken}`)
@@ -102,4 +124,60 @@ describe("INTEGRATION: ShowtimeControler.create - POST /api/v1/showtimes", () =>
     const showtimeCount = await showtimeRepository.count();
     expect(showtimeCount).toBe(0);
   });
+
+  test("should return a 404 when creating a showtime with non-existent room id", async () => {
+    const showtimeWithInvalidRoomId = {
+      ...showtimeCreateInput,
+      roomId: randomUUID(),
+    };
+
+    const response = await apiClient
+      .post(showtimeEndpoint)
+      .set("Authorization", `Bearer ${adminUserToken}`)
+      .send(showtimeWithInvalidRoomId);
+
+    const expectedResponseBody = {
+      details: "Room not found",
+    };
+
+    expect(response.status).toBe(status.HTTP_404_NOT_FOUND);
+    expect(response.body).toEqual(expectedResponseBody);
+
+    const showtimeCount = await showtimeRepository.count();
+    expect(showtimeCount).toBe(0);
+  });
+
+  test("should return a 404 when creating a showtime with non-existent movie id", async () => {
+    const showtimeWithInvalidMovieId = {
+      ...showtimeCreateInput,
+      movieId: randomUUID(),
+    };
+
+    const response = await apiClient
+      .post(showtimeEndpoint)
+      .set("Authorization", `Bearer ${adminUserToken}`)
+      .send(showtimeWithInvalidMovieId);
+
+    const expectedResponseBody = {
+      details: "Movie not found",
+    };
+
+    expect(response.status).toBe(status.HTTP_404_NOT_FOUND);
+    expect(response.body).toEqual(expectedResponseBody);
+
+    const showtimeCount = await showtimeRepository.count();
+    expect(showtimeCount).toBe(0);
+  });
+
+  test.todo(
+    "should return a 400 when creating a showtime with invalid datetime"
+  );
+  test.todo(
+    "should return a 400 when creating a showtime with invalid datetime format"
+  );
+  test.todo(
+    "should return a 422 when creating a showtime with a datetime in the past"
+  );
+
+  test.todo("should create a showtime with admin user credentials");
 });
