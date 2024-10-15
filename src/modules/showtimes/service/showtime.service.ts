9768946efb885aee8ac70d3b5e23e214d3b27d86
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-15 15:44:23 -0300

    fix: :bug: add validation to room id not found in showtime service create

--- src/modules/showtimes/service/showtime.service.ts
+++ src/modules/showtimes/service/showtime.service.ts
@@ -3,6 +3,7 @@ import type { IShowtimeRepository } from "../repository";
 import type { Showtime, ShowtimeCreateInput } from "../types";
 import type { IShowtimeService } from "./showtime.service.interface";
 import type { IMovieRepository } from "modules/movies/repository";
+import { RoomNotFoundError } from "modules/rooms/errors";
 
 export class ShowtimeService implements IShowtimeService {
   constructor(
@@ -12,6 +13,14 @@ export class ShowtimeService implements IShowtimeService {
   ) {}
 
   async create(showtimeCreateInput: ShowtimeCreateInput): Promise<Showtime> {
+    const roomCount = await this.roomRepository.countById(
+      showtimeCreateInput.roomId
+    );
+
+    if (roomCount === 0) {
+      throw new RoomNotFoundError();
+    }
+
     return await this.showtimeRepository.create(showtimeCreateInput);
   }
 }
