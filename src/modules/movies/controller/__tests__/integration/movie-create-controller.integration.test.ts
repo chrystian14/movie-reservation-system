f808be167c50b988adc87e7ea55814008ae69d87
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-15 16:29:45 -0300

    test: :white_check_mark: add movie controller create integration test for non existent genre id

--- src/modules/movies/controller/__tests__/integration/movie-create-controller.integration.test.ts
+++ src/modules/movies/controller/__tests__/integration/movie-create-controller.integration.test.ts
@@ -1,5 +1,6 @@
 import { clearDatabase } from "configs/jest-setup.config";
 import { prisma } from "configs/prisma-client.config";
+import { randomUUID } from "crypto";
 import { apiClient } from "modules/_shared/tests";
 import { status } from "modules/_shared/utils";
 import { generateToken } from "modules/auth/jwt";
@@ -110,6 +111,28 @@ describe("INTEGRATION: MovieControler.create - POST /api/v1/movies", () => {
     };
 
     expect(response.status).toBe(status.HTTP_400_BAD_REQUEST);
+    expect(response.body).toEqual(expectedResponseBody);
+
+    const movieCount = await prisma.movie.count();
+    expect(movieCount).toBe(0);
+  });
+
+  test("should return a 404 when creating a movie with non-existent genre id", async () => {
+    const movieWithInvalidGenreId = {
+      ...movieWithValidGenre,
+      genreId: randomUUID(),
+    };
+
+    const response = await apiClient
+      .post(movieEndpoint)
+      .set("Authorization", `Bearer ${adminUserToken}`)
+      .send(movieWithInvalidGenreId);
+
+    const expectedResponseBody = {
+      details: "Genre not found",
+    };
+
+    expect(response.status).toBe(status.HTTP_404_NOT_FOUND);
     expect(response.body).toEqual(expectedResponseBody);
 
     const movieCount = await prisma.movie.count();
