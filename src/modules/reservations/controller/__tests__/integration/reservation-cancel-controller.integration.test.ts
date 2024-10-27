0d043c3f04eafc9ecd49a7d7d729f7d885d99bab
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-27 14:26:55 -0300

    test: :white_check_mark: add reservation cancel test for user not owner of reservation

--- src/modules/reservations/controller/__tests__/integration/reservation-cancel-controller.integration.test.ts
+++ src/modules/reservations/controller/__tests__/integration/reservation-cancel-controller.integration.test.ts
@@ -9,10 +9,7 @@ import { GenreRepository } from "modules/genres/repository";
 import { MovieBuilder } from "modules/movies/builder";
 import { MovieRepository } from "modules/movies/repository";
 import { ReservationBuilder } from "modules/reservations/builder";
-import {
-  ReservationRepository,
-  type IReservationRepository,
-} from "modules/reservations/repository";
+import { ReservationRepository } from "modules/reservations/repository";
 import { RoomBuilder } from "modules/rooms/builder";
 import { RoomRepository } from "modules/rooms/repository";
 import { SeatRepository } from "modules/seats/repository";
@@ -23,20 +20,19 @@ import { UserBuilder } from "modules/users/builder";
 import { UserRepository } from "modules/users/repository";
 import type { User } from "modules/users/types";
 
-describe("INTEGRATION: ReservationControler.cancel - DEL /api/v1/reservations", () => {
+describe("INTEGRATION: ReservationControler.cancel - DEL /api/v1/reservations/:id", () => {
   const reservationEndpoint = "/api/v1/reservations";
 
   let regularUserOne: User;
   let regularUserOneToken: string;
 
   let regularUserTwo: User;
+  let regularUserTwoToken: string;
 
   let savedRoomOne: Room;
   let savedSeatsForRoomOne: Seat[];
 
   let savedShowtimeOne: Showtime;
-
-  let reservationRepository: IReservationRepository;
 
   beforeEach(async () => {
     await clearDatabase();
@@ -48,6 +44,7 @@ describe("INTEGRATION: ReservationControler.cancel - DEL /api/v1/reservations", 
 
     const regularUserTwoBuilder = new UserBuilder().withNonAdminRole();
     regularUserTwo = await regularUserTwoBuilder.save(userRepository);
+    regularUserTwoToken = generateToken(regularUserTwo);
 
     const createdGenre = await new GenreBuilder().save(new GenreRepository());
     const createdMovie = await new MovieBuilder()
@@ -92,6 +89,29 @@ describe("INTEGRATION: ReservationControler.cancel - DEL /api/v1/reservations", 
     };
 
     expect(response.statusCode).toBe(status.HTTP_404_NOT_FOUND);
+    expect(response.body).toStrictEqual(expectedResponseBody);
+  });
+
+  test("should return a 403 when user is not the owner of the reservation", async () => {
+    const [userOneSeatOne, ..._restSeats] = savedSeatsForRoomOne;
+
+    const [userOneReservationOne] = await new ReservationBuilder()
+      .withUserId(regularUserOne.id)
+      .withSeatIds([userOneSeatOne.id])
+      .withShowtimeId(savedShowtimeOne.id)
+      .save(new ReservationRepository());
+
+    const reservationCancelEndpoint =
+      reservationEndpoint + "/" + userOneReservationOne.id;
+    const response = await apiClient
+      .delete(reservationCancelEndpoint)
+      .set("Authorization", `Bearer ${regularUserTwoToken}`);
+
+    const expectedResponseBody = {
+      details: "You don't have permission to perform this action",
+    };
+
+    expect(response.statusCode).toBe(status.HTTP_403_FORBIDDEN);
     expect(response.body).toStrictEqual(expectedResponseBody);
   });
 });
