d49173065efaf0ba7c8fcc8d1f78d22456eaad9b
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-10-28 23:20:12 -0300

    fix: :bug: update user password schema and docs

--- src/modules/users/types/schemas.ts
+++ src/modules/users/types/schemas.ts
@@ -4,7 +4,7 @@ export const userSchema = z.object({
   id: z.string().uuid(),
   firstName: z.string().min(1).max(50),
   lastName: z.string().min(1).max(50),
-  email: z.string().email(),
+  email: z.string().email().max(255),
   password: z.string().min(6).max(64),
   isAdmin: z.boolean().default(false),
   createdAt: z.date(),
